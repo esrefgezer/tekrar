7c2ad3391b68adf1830256df5df75cc66f2f84a6
esrefgezer <esrefgezer@yahoo.com>
2024-06-12 22:54:33 +0200

    first commit

---
+++ src/day01_PracticeStart/P01_HelloWorld.java
@@ -0,0 +1,12 @@
+package day01_PracticeStart;
+
+public class P01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello Wise --");
+        System.out.println("Have a wise day");
+    }
+}
+
+
+
